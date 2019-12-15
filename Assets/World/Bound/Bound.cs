ef9abb958a95a65fa29ca9f5a8e711d1997f0483
ema.bolo <ema.bolo94@gmail.com>
2019-12-16 00:12:13 +0100

    space skybox in all scenes

--- Assets/World/Bound/Bound.cs
+++ Assets/World/Bound/Bound.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bound : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
-        GameController.loadRelativeLevel(0); // reset level
+        if (other.CompareTag("Player"))
+            GameController.loadRelativeLevel(0); // reset level
     }
 }
