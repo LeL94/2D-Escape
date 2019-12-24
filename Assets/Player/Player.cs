66cae4b8bb4426cd6e714483f051f155b4e0f79e
ema.bolo <ema.bolo94@gmail.com>
2019-12-24 15:45:58 +0100

    improved player jump

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     [SerializeField] float moveSpeed = 10f;
-    [SerializeField] float jumpForce = 15f;
+    [SerializeField] float jumpForce = 17f;
     [SerializeField] float gravityForce = 3f;
 
     private Rigidbody rb;
     private SphereCollider myCollider;
     private float distToGround;
+
+    // improve jumping: when press jump before being grounded
+    private float jumpPressedRemember = 0f;
+    private float jumpPressedRememberTime = 0.2f;
+
+    // improve jumping: when press jump after being grounded
+    private float groundedRembember = 0f;
+    private float groundedRembemberTime = 0.2f;
 
     private void Start() {
         rb = GetComponent<Rigidbody>();
@@ -46,20 +54,42 @@ public class Player : MonoBehaviour {
 
     }
 
-    private void Jump() {       
+    private void Jump() {
+        if (!GameController.getIsJumpEnabled())
+            return;
 
-        if (GameController.getIsJumpEnabled() && Input.GetButtonDown("Jump")) {
 
-            if (GameController.getIsGravityInverted()) {
-                if (!isGrounded("above"))
-                    return;
+        // jumping even if jump button was pressed 0.2s ago
+        if (jumpPressedRemember > 0)
+            jumpPressedRemember -= Time.deltaTime;
+
+        // jumping even if was grounded 0.2s ago
+        if (groundedRembember > 0)
+            groundedRembember -= Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump"))
+            jumpPressedRemember = jumpPressedRememberTime;
+
+        // gravity normal
+        if (!GameController.getIsGravityInverted()) {
+            if (isGrounded("below"))
+                groundedRembember = groundedRembemberTime;
+
+            if (groundedRembember > 0 && jumpPressedRemember > 0) {
+                jumpPressedRemember = 0f;
+                groundedRembember = 0f;
+                rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            }
+        }
+        // gravity inverted
+        else {
+            if (isGrounded("above"))
+                groundedRembember = groundedRembemberTime;
+
+            if (groundedRembember > 0 && jumpPressedRemember > 0) {
+                jumpPressedRemember = 0f;
                 rb.velocity = new Vector3(rb.velocity.x, -jumpForce, rb.velocity.z);
             }
-            else {
-                if (!isGrounded("below"))
-                    return;
-                rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
-            }            
         }
     }
 
@@ -72,10 +102,9 @@ public class Player : MonoBehaviour {
             return Physics.Raycast(transform.position, Vector3.up, distToGround + 0.1f);
 
         else
-            Debug.Log("is grounded direction note recognized");
+            Debug.Log("Player: is grounded direction not recognized");
         return false;
     }
-
 }
 
 /* Character controller
