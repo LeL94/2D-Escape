ad93d2d9e9117e0f6a61c5dc7ff0fbb2c98a7fe8
ema.bolo <ema.bolo94@gmail.com>
2019-12-29 21:22:20 +0100

    button can trigger

--- Assets/Prefabs/Environment/Hazards/MovingHazard.cs
+++ Assets/Prefabs/Environment/Hazards/MovingHazard.cs
@@ -2,35 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingHazard : MonoBehaviour
+public class MovingHazard : TriggerListener
 {
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private bool isTriggered = true;
 
     public Transform point1, point2;
 
-    private Vector3 startPoint, targetPoint; // current start point (to handle Lerp movement)
+    private Vector3 startPos, targetPos; // current start point (to handle Lerp movement)
     private float timePosition = 0f; // current time position (to handle Lerp movement)
 
     private void Start() {
-        transform.position = point1.position; // move hazard at point 1
-        startPoint = point1.position;
-        targetPoint = point2.position;
+        startPos = transform.position;
+        targetPos = point2.position;
     }
 
     private void Update() {
-        Move();
+        if (isTriggered)
+            Move();
     }
 
     private void Move() {
         timePosition += Time.deltaTime;
-        transform.position = Vector3.Lerp(startPoint, targetPoint, timePosition * moveSpeed);
+        transform.position = Vector3.Lerp(startPos, targetPos, timePosition * moveSpeed);
 
-        if (Vector3.Distance(transform.position, targetPoint) <= 0.01f) {
+        if (Vector3.Distance(transform.position, targetPos) <= 0.01f) {
             timePosition = 0;
 
-            Vector3 temp = startPoint;
-            startPoint = targetPoint;
-            targetPoint = temp;
+            if (targetPos == point1.position) {
+                startPos = point1.position;
+                targetPos = point2.position;
+            }
+            else {
+                startPos = point2.position;
+                targetPos = point1.position;
+            }
         }
+    }
+
+    override public void TriggerAction() {
+        isTriggered = true;
     }
 }
---
+++ Assets/Prefabs/Interactibles/Button/ButtonSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSwitch : MonoBehaviour
+{
+    [SerializeField] private int sfxIndex = 6;
+
+    public TriggerListener triggerListener;
+
+    private bool isPressed = false;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player") && !isPressed) {
+            isPressed = true;
+            transform.localPosition = Vector3.zero;
+            AudioManager.instance.PlaySFX(sfxIndex);
+
+            // trigger action
+            triggerListener.TriggerAction();
+        }
+    }
+}
